6d6757cfbf639710897ee96dfede7c6f0f249910
wisdomateke-maker <wisdomateke@gmail.com>
2026-09-11 12:50:11 +0100

    Complete: Write a function that raises a number to a power. If no powe

--- labs/main.py
+++ labs/main.py
@@ -1,5 +1,4 @@
-def total_word_count(sentences):
-    total = 0
-    for sentence in sentences:
-        total += len(sentence.split())
-    return total
+def power_of(base, exponent=2):
+    # TODO: return base raised to the power of exponent
+    return base **  exponent
+print(power_of(5))
