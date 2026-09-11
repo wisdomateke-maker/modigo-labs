1c03e7ec0dffc7e430c2590a89265ed7af61cfa7
wisdomateke-maker <wisdomateke@gmail.com>
2026-09-11 13:24:20 +0100

    Complete: Below is a function meant to generate a greeting card messag

--- labs/main.py
+++ labs/main.py
@@ -1,5 +1,6 @@
-def format_line_item(description, amount, tax_rate=7.5, currency="$"):
-    # TODO: calculate the taxed total using tax_rate, round to 2 decimal places,
-    # and return "{description}: {currency}{total}"
-    total = round(amount + (amount * tax_rate /100), 2)
-    return f"{description}: {currency}{total}"
+def create_greeting(recipient_name, sender_name="A Friend", closing="Best wishes"):
+    # TODO: return the formatted greeting string using recipient_name,
+    # sender_name, and closing
+    return f"Dear {recipient_name}, {closing}! From, {sender_name}."
+print(create_greeting("Ada"))
+print(create_greeting("Bola", sender_name="chidi"))
